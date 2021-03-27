689da6a63a6c2a859f056975c0085f984b56aa1a
Christian <schauer@inf-schauer.de>
2021-03-27 17:12:57 +0100

    ...

---
+++ src/main/java/de/inf_schauer/moba_gui/MobaGuiApplication.java
@@ -0,0 +1,13 @@
+package de.inf_schauer.moba_gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobaGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobaGuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/inf_schauer/moba_gui/MobaGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.inf_schauer.moba_gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobaGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
